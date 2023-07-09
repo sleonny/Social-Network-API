80e3fd4a26393fd711f25fd1718e91de9c838cbf
Sean L <leonardmsean@gmail.com>
2023-07-09 17:25:39 -0400

    added Reaction Schema

---
+++ models/Reaction.js
@@ -0,0 +1,28 @@
+const { Schema, Types } = require("mongoose");
+const moment = require("moment");
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = ReactionSchema;
