9e4a27046cd52fac58fe70d0942eb023d2808445
Sean L <leonardmsean@gmail.com>
2023-07-13 18:36:43 -0400

    coded createUser controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,4 +34,9 @@ const userController = {
         console.log(err);
       });
   },
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
 };
