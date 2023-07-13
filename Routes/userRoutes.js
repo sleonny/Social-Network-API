31b7686eeaee1abc1ce59f0bc0e4073e24903a1c
Sean L <leonardmsean@gmail.com>
2023-07-13 16:55:57 -0400

    finished user routes

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+
 const {
   getAllUsers,
   getUserByID,
@@ -8,3 +9,11 @@ const {
   addFriend,
   removeFriend,
 } = require("../controllers/userController");
+
+router.route("/").get(getAllUsers).post(createUser);
+
+router.route("/:id").get(getUserByID).put(updateUser).delete(deleteUser);
+
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
+module.exports = router;
