1e6b4a5b667cedddd92a8846c250ba05dc89f2d7
Sean L <leonardmsean@gmail.com>
2023-07-13 18:09:38 -0400

    added getUserById route

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,4 +18,20 @@ const userController = {
         res.status(500).json(err);
       });
   },
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .populate({
+        path: "friends",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+      });
+  },
 };
