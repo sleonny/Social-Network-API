f6dd185e327b6f44c3bed5e62c2d2d04ea4bd58f
Sean L <leonardmsean@gmail.com>
2023-07-14 08:58:44 -0400

    added createthought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -24,4 +24,23 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+  createThought({ body }, res) {
+    Thought.create(body)
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { _id: body.userId },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res
+            .status(404)
+            .json({ message: "No user found with this ID" });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
 };
