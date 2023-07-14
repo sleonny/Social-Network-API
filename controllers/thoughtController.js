8aacb25179d3f7a4065f2605988b4826706c4734
Sean L <leonardmsean@gmail.com>
2023-07-14 08:40:27 -0400

    added getallthoughts controller

---
+++ controllers/thoughtController.js
@@ -0,0 +1,12 @@
+const { Thought, User } = require("../models");
+
+const thoughtController = {
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
