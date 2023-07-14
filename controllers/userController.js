f5bc27381782864cd37dddc58bfd0392003572fc
Sean L <leonardmsean@gmail.com>
2023-07-13 20:45:01 -0400

    finished up userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -80,4 +80,15 @@ const userController = {
       })
       .catch((err) => res.json(err));
   },
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
+  },
 };
+
+module.exports = userController;
