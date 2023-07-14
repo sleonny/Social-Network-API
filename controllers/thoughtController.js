7f2c94bdbb9e800032d4cbdd74aa3ed815d0ca0e
Sean L <leonardmsean@gmail.com>
2023-07-14 09:08:31 -0400

    built addReaction controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -63,4 +63,20 @@ const thoughtController = {
       .then((dbThoughtData) => res.json(dbThoughtData))
       .catch((err) => res.status(400).json(err));
   },
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res
+            .status(404)
+            .json({ message: "No thought found with this ID" });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  },
 };
