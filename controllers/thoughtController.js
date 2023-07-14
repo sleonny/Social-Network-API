5ab9d023b6401be7433f0ae95804702f5dc87b1b
Sean L <leonardmsean@gmail.com>
2023-07-14 08:49:33 -0400

    added getthoughtbyid controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -9,4 +9,19 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res
+            .status(404)
+            .json({ message: "No thought found with this ID" });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 };
