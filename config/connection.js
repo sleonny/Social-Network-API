0239039b5ad4c392b4f7581eaf8d3efb9ac6ca57
Sean L <leonardmsean@gmail.com>
2023-07-15 15:04:25 -0400

    changed connection.js for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/mygroceryDB');
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.127017/Social-NetworkDB"
+);
 
 module.exports = mongoose.connection;
