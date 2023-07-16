fcdba1c21aeb23427dea02e4582d69247cb14291
Sean L <leonardmsean@gmail.com>
2023-07-16 13:54:33 -0400

    changing path of server to see if that helps

--- server.js
+++ server.js
@@ -3,17 +3,9 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/Social-NetworkDB",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
 const db = mongoose.connection;
 
-const PORT = process.env.PORT || 27017;
+const PORT = process.env.PORT || 3000;
 
 const userRoutes = require("./Routes/userRoutes");
 const thoughtRoutes = require("./Routes/thoughtRoutes");
@@ -21,6 +13,23 @@ const thoughtRoutes = require("./Routes/thoughtRoutes");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(userRoutes, thoughtRoutes);
+
+const User = require("./models/User"); // assuming you have a User model defined
+
+const sampleUser = new User({
+  username: "Sample User",
+  email: "sampleuser@test.com",
+  // other fields according to your schema
+});
+
+sampleUser
+  .save()
+  .then((doc) => {
+    console.log("User saved successfully", doc);
+  })
+  .catch((err) => {
+    console.log("Error occurred while saving user", err);
+  });
 
 db.once("open", () => {
   app.listen(PORT, () => {
