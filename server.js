891df3cb5d24f150c43efaece75d4b26bfd50a12
Sean L <leonardmsean@gmail.com>
2023-07-16 14:00:56 -0400

    new approach to server starting

--- server.js
+++ server.js
@@ -1,38 +1,17 @@
 const express = require("express");
-const mongoose = require("mongoose");
-
-const app = express();
-
-const db = mongoose.connection;
-
-const PORT = process.env.PORT || 3000;
-
+const db = require("./config/connection");
 const userRoutes = require("./Routes/userRoutes");
 const thoughtRoutes = require("./Routes/thoughtRoutes");
 
-app.use(express.json());
+const PORT = process.env.PORT || 3000;
+const app = express();
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(userRoutes, thoughtRoutes);
-
-const User = require("./models/User"); // assuming you have a User model defined
-
-const sampleUser = new User({
-  username: "Sample User",
-  email: "sampleuser@test.com",
-  // other fields according to your schema
-});
-
-sampleUser
-  .save()
-  .then((doc) => {
-    console.log("User saved successfully", doc);
-  })
-  .catch((err) => {
-    console.log("Error occurred while saving user", err);
-  });
 
 db.once("open", () => {
   app.listen(PORT, () => {
-    console.log("Listening on that port ${PORT}!");
+    console.log(`That server is running on port ${PORT}!`);
   });
 });
